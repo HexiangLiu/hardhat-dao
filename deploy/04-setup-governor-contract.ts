7a0ebe294b4b4dd153f1b780c4ac2854c38ea6b8
刘鹤翔 <liuhexiang@bytedance.com>
2023-11-28 19:14:30 +0800

    feat: setup proposer and revoke admin

---
+++ deploy/04-setup-governor-contract.ts
@@ -0,0 +1,23 @@
+import { DeployFunction } from 'hardhat-deploy/dist/types';
+import { ethers } from 'hardhat';
+import { TimeLock } from '../typechain-types';
+
+const setup: DeployFunction = async ({
+  deployments: { log },
+  getNamedAccounts,
+}) => {
+  const { deployer } = await getNamedAccounts();
+  const GovernorContract = await ethers.getContract('GovernorContract');
+  const governorContractAddress = await GovernorContract.getAddress();
+  const TimeLock: TimeLock = await ethers.getContract('TimeLock', deployer);
+  const PROPOSER_ROLE = await TimeLock.PROPOSER_ROLE();
+  const DEFAULT_ADMIN_ROLE = await TimeLock.DEFAULT_ADMIN_ROLE();
+  log('---------SETUP----------');
+  const tx1 = await TimeLock.grantRole(PROPOSER_ROLE, governorContractAddress);
+  await tx1.wait();
+  const tx2 = await TimeLock.revokeRole(DEFAULT_ADMIN_ROLE, ethers.ZeroAddress);
+  await tx2.wait();
+  log('---------DONE-----------');
+};
+
+export default setup;
